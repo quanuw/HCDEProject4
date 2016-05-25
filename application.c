bfcba7d3fae7385c8f2c38bac0085c4f99a016e4
bradleymarquez <bradleymarquez@hotmail.com>
2016-05-24 17:35:26 -0700

    updated

--- application.c
+++ application.c
@@ -1,37 +1,54 @@
 // Brandon Ngo, Brad Marquez, Quan Nguyen
 // EE 371 - Lab 4
 //
-// networkInterface.c
+// application.c
 #include "sys/alt_stdio.h"
 #include "altera_avalon_pio_regs.h"
 #define DATA_SIZE 16
 #define BUFFER_SIZE 4
 #define TIME_OUT 150000
  
-#define leds (volatile char *) 0x3020
-// #define sendAddress (volatile char *) 
-// #define receiveAddress (volatile char *) 
-// #define charReceived (volatile char *) 
+#define leds (volatile char *) 0x3050
+#define sendAddress (volatile char *) 0x3080
+#define receiveAddress (volatile char *) 0x3020
+#define charReceived (volatile char *) 0x3030
+#define charSent (volatile char *) 0x3040
+#define transmitEnable (volatile char *) 0x3060
+#define load (volatile char *) 0x3070
 
 // IOWR_ALTERA_AVALON_PIO_DATA(targetAddress, aValue); : WRITE
-// aValue = IOWR_ALTERA_AVALON_PIO_DATA(sourceAddress); : READ
+// aValue = IORD_ALTERA_AVALON_PIO_DATA(sourceAddress); : READ
+void write(char);
 int main() {
-		command = ' ';
+		char command = ' ';
 		while (command != 37) { // escape
 		   alt_putstr("Enter an ASCII character: ");
-		   while (command2 >= 0 && command2 < 256) {
-				command2 = alt_getchar();
+		   while (command >= 0 && command < 128) {
+				command = alt_getchar();
 				alt_getchar();
-				if(command2 < 0 || command2 >= 256) {
+				if(command < 0 || command >= 128) {
 					alt_putstr("\nThis is not a valid character. Please enter another character. \n");
 				} else {
-					alt_printf("Transmitted Character: %c\n", command2);
+					alt_printf("Transmitted Character: %c\n", command);
 				}
 			}
-			IOWR_ALTERA_AVALON_PIO_DATA(sendAddress, command2 + '0');
-			*leds = *IOWR_ALTERA_AVALON_PIO_DATA(sendAddress);
+
+			write(command);
+			// Read data
+			*leds = *IORD_ALTERA_AVALON_PIO_DATA(sendAddress);
 			alt_printf("Received Character: %c\n", receiveAddress);
 		}
 	}
 	return 0;
 }
+
+// Writes given character
+void write(char command) {
+	IOWR_ALTERA_AVALON_PIO_DATA(sendAddress, command + '0');
+	IOWR_ALTERA_AVALON_PIO_DATA(load, 1);
+	IOWR_ALTERA_AVALON_PIO_DATA(transmitEnable, 1);
+	usleep(4);
+	IOWR_ALTERA_AVALON_PIO_DATA(load, 0);
+	IOWR_ALTERA_AVALON_PIO_DATA(transmitEnable, 0);
+}
+
